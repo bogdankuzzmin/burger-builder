4ad8605db66ffd38b4f8f0fe59998df36f6fb0b3
Bogdan <bogdankuzzmin@gmail.com>
2021-01-25 23:50:50 +0600

    App.js - added Layout and BurgerBuilder to App - created class component instead of function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App() {
-  return (
-    <div className="App">
-      <p>Burger Builder</p>
-    </div>
-  );
+import React, {Component} from 'react';
+
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Burger Builder</p>
+        <Layout />
+        <BurgerBuilder />
+      </div>
+    );
+  }
 }
 
 export default App;
