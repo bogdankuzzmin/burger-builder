7f92ab027d93558eb338720487736eaa8c20671d
Bogdan <bogdankuzzmin@gmail.com>
2021-01-27 10:08:56 +0600

    - added opportunities to add / remove ingredients by press a button - added prices for ingredients and totalPrice - added a button which clears all ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,8 +14,14 @@ const buildControls = (props) => {
   return (
     <div className={classes.BuildControls}>
       {controls.map((control) => {
-        return <BuildControl key={control.label} label={control.label }/>
+        return <BuildControl
+                  key={control.label}
+                  label={control.label}
+                  added={() => props.ingredientAdded(control.type)}
+                  removed={() => props.ingredientRemoved(control.type)}
+                  disabled={props.disabled[control.type]} />
       })}
+      <button onClick={props.ingredientCleared}>Clear</button>
     </div>
   );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import React, {Component, Fragment} from 'react';
 import Burger from '../../components/Burger';
 import BuildControls from '../../components/Burger/BuildControls/';
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  bacon: 0.7,
+  cheese: 0.4,
+  meat: 1.2,
+};
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -10,14 +17,79 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0,
-    }
+    },
+    totalPrice: 4,
   }
 
+  addIngredientHandler = (type) => {
+    const oldIngredient = this.state.ingredients[type];
+    const updateIngredient = oldIngredient + 1;
+
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+
+    this.setState((state) => {
+      return {
+        ingredients: {
+          ...state.ingredients,
+          [type]: updateIngredient
+        },
+        totalPrice: newPrice,
+      }
+    });
+  };
+
+  removeIngredientHandler = (type) => {
+    const oldIngredient = this.state.ingredients[type];
+    if (oldIngredient <= 0) {
+      return;
+    }
+    const updateIngredient = oldIngredient - 1;
+
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+
+    this.setState((state) => {
+      return {
+        ingredients: {
+          ...state.ingredients,
+          [type]: updateIngredient,
+        },
+        totalPrice: newPrice,
+      }
+    });
+  };
+
+  clearIngredientHandler = () => {
+    this.setState({
+      ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0,
+      }
+    });
+  };
+
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
     return (
       <Fragment>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          ingredientCleared={this.clearIngredientHandler}
+          disabled={disabledInfo} />
       </Fragment>
     );
   }
