01e7dd0f28020225424c57fd38766635c8aad58e
Bogdan <bogdankuzzmin@gmail.com>
2021-02-07 21:25:46 +0600

    - removed Input style from ContactData - added Input to ContactData instead of <input ...

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import axios from '../../../axious-orders';
 import classes from './ContactData.module.css';
 import Button from '../../../components/UI/Button';
 import Spinner from '../../../components/UI/Spinner';
+import Input from '../../../components/UI/Input';
 
 class ContactData extends Component {
   state  = {
@@ -50,10 +51,10 @@ class ContactData extends Component {
   render() {
     let form = (
       <form action="">
-        <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-        <input className={classes.Input} type="text" name="email" placeholder="Your Mail" />
-        <input className={classes.Input} type="text" name="street" placeholder="Street" />
-        <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+        <Input inputtype='input' type="text" name="name" placeholder="Your Name" />
+        <Input inputtype='input' type="text" name="email" placeholder="Your Mail" />
+        <Input inputtype='input' type="text" name="street" placeholder="Street" />
+        <Input inputtype='input' type="text" name="postal" placeholder="Postal Code" />
         <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
       </form>
     );
