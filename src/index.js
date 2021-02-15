d668922b98f9963ae493754371240005544b41ec
Bogdan <bogdankuzzmin@gmail.com>
2021-02-15 16:51:13 +0600

    installed redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import {BrowserRouter} from 'react-router-dom';
 
 import './index.css';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import App from './App';
 
-const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const store = createStore(burgerBuilderReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 ReactDOM.render(
   <Provider store={store}>
