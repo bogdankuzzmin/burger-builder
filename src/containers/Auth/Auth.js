18ca8629befda1de5fb5314257bb69e33ebe898e
Bogdan <bogdankuzzmin@gmail.com>
2021-02-20 23:19:48 +0600

    [Auth] - get expiresIn and added authLogout action

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import classes from './Auth.module.css';
 
 import Input from '../../components/UI/Input';
 import Button from '../../components/UI/Button';
+import Spinner from '../../components/UI/Spinner';
 
 import * as actions from '../../store/actions';
 
@@ -109,7 +110,7 @@ class Auth extends Component {
       });
     }
 
-    const form = formElementsArray.map((formElement) => (
+    let form = formElementsArray.map((formElement) => (
       <Input
         key={formElement.id}
         elementType={formElement.config.elementType}
@@ -122,8 +123,21 @@ class Auth extends Component {
         required={formElement.config.validation.required} />
     ));
 
+    if (this.props.loading) {
+      form = <Spinner />;
+    }
+
+    let errorMessage = null;
+
+    if (this.props.error) {
+      errorMessage = (
+        <p>{this.props.error.message}</p>
+      );
+    }
+
     return (
       <div className={classes.Auth}>
+        {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">Submit</Button>
@@ -138,10 +152,17 @@ class Auth extends Component {
   }
 }
 
+const mapStateToProps = (state) => {
+  return  {
+    loading: state.auth.loading,
+    error: state.auth.error,
+  };
+};
+
 const mapDispatchToProps = (dispatch) => {
   return {
     authHandler: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
-  }
+  };
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
