c836ee849dd8e088e52858364dae9f94cbde8e18
Bogdan <bogdankuzzmin@gmail.com>
2021-02-24 22:23:47 +0600

    added lazy loading for Checkout, Auth, Orders

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -55,12 +55,11 @@ export const auth = (email, password, isSignup) => {
     }
     axios.post(url, authData)
       .then((response) => {
-
         const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
         localStorage.setItem('token', response.data.idToken);
         localStorage.setItem('expirationDate', expirationDate);
         localStorage.setItem('userId', response.data.localId);
-
+        console.log(response);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
